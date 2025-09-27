974f892c828c65cde9902d9d5f8ffe383a669212
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-09-27 20:20:13 +0530

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,27 +1,32 @@
-from flask import Flask, request
+from flask import Flask, jsonify
 import mysql.connector
 import os
 
 app = Flask(__name__)
 
-db_config = {
-    "host": os.getenv("DB_HOST", "db"),
-    "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "rootpassword"),
-    "database": os.getenv("DB_NAME", "myappdb"),
-}
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "rootpassword")
+DB_NAME = os.getenv("DB_NAME", "myappdb")
 
-@app.route('/')
+@app.route("/")
 def home():
     try:
-        conn = mysql.connector.connect(**db_config)
+        conn = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
         cursor = conn.cursor()
-        cursor.execute("SELECT NOW()")
+        cursor.execute("SELECT message FROM greetings LIMIT 1;")
         result = cursor.fetchone()
+        cursor.close()
         conn.close()
-        return f"Hello from Python App! DB time: {result[0]}"
+        return jsonify({"message": result[0]})
     except Exception as e:
-        return f"Error: {str(e)}"
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
