1fc06d78cd6a7e681409de55b1953cd3c5fd8f4c
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-09-30 15:34:31 +0530

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,9 @@
+CREATE DATABASE IF NOT EXISTS myappdb;
+USE myappdb;
+
 CREATE TABLE IF NOT EXISTS greetings (
     id INT AUTO_INCREMENT PRIMARY KEY,
     message VARCHAR(255) NOT NULL
 );
 
-INSERT INTO greetings (message) VALUES ("Hello from MySQL + Flask + Docker!");
-
+INSERT INTO greetings (message) VALUES ('Welcome to Dockerized MySQL!');
